fc943f2fa7a6b0108e69b1ec6701c627dace69a8
Burak Yazar <b21605362@cs.hacettepe.edu.tr>
2018-11-29 23:07:34 +0300

    -Main structure of the program created.

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,21 @@
 import sys
 import io
 
+# Get input file via arguments
+fileName = sys.argv[1]
+
+# Create data structures needed
+
+# Open input file for reading
+inText = open(fileName, 'r')
+
+# Main loop for reading line by line
+while True:
+    # currentLine is the line read by interpretative
+    currentLine = inText.readline()
+
+    # Parse commands and process them
+
+    # End of file
+    if not currentLine:
+        exit(0)
